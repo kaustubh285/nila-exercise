cf3a23801295453ff62f139dea88955e5231a925
kaustubh285 <kaustubh285@gmail.com>
2025-06-20 16:54:21 +0100

    feat: add task schema

---
+++ packages/api/drizzle/0001_furry_thaddeus_ross.sql
@@ -0,0 +1,12 @@
+CREATE TYPE "public"."task_priority" AS ENUM('low', 'medium', 'high');--> statement-breakpoint
+CREATE TYPE "public"."task_status" AS ENUM('todo', 'in_progress', 'done');--> statement-breakpoint
+CREATE TABLE "tasks" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" text NOT NULL,
+	"description" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"project_id" text NOT NULL,
+	"status" "task_status" DEFAULT 'todo' NOT NULL,
+	"priority" "task_priority" DEFAULT 'medium' NOT NULL
+);
--- packages/api/src/db/schemas/project.schema.ts
+++ packages/api/src/db/schemas/project.schema.ts
@@ -1,6 +1,7 @@
 import { relations } from 'drizzle-orm';
 import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 import { usersTable } from './user.schema';
+import { tasksTable } from './task.schema';
 
 export const projectsTable = pgTable('projects', {
 	id: uuid('id').defaultRandom().primaryKey(),
@@ -15,9 +16,10 @@ export const projectsTable = pgTable('projects', {
 	deletedAt: timestamp('deleted_at'),
 });
 
-export const projectsRelations = relations(projectsTable, ({ one }) => ({
+export const projectsRelations = relations(projectsTable, ({ one, many }) => ({
 	user: one(usersTable, {
 		fields: [projectsTable.userId],
 		references: [usersTable.id],
 	}),
+	tasks: many(tasksTable),
 }));
